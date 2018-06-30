fcad056769cad83b80de1e366214bdb92a2973c8
jackyrusly <jackyrusly@gmail.com>
2018-06-30 08:10:39 +0700

    Responsive

--- src/components/Social/Social.js
+++ src/components/Social/Social.js
@@ -3,11 +3,11 @@ import './Social.scss';
 
 const Social = () => (
     <div id="social-container">
-        <a href="https://github.com/jackyrusly" rel="noreferrer noopener" target="_blank"><i className="fab fa-github fa-5x"></i></a>
-        <a href="https://www.facebook.com/jackyrusly" rel="noreferrer noopener" target="_blank"><i className="fab fa-facebook-square fa-5x"></i></a>
-        <a href="https://plus.google.com/+JackyRuslyJR" rel="noreferrer noopener" target="_blank"><i className="fab fa-google-plus-square fa-5x"></i></a>
-        <a href="https://www.instagram.com/jackyrusly/" rel="noreferrer noopener" target="_blank"><i className="fab fa-instagram fa-5x"></i></a>
-        <a href="https://www.linkedin.com/in/jacky-rusly" rel="noreferrer noopener" target="_blank"><i className="fab fa-linkedin fa-5x"></i></a>
+        <a href="https://github.com/jackyrusly" rel="noreferrer noopener" target="_blank"><i className="fab fa-github"></i></a>
+        <a href="https://www.facebook.com/jackyrusly" rel="noreferrer noopener" target="_blank"><i className="fab fa-facebook-square"></i></a>
+        <a href="https://plus.google.com/+JackyRuslyJR" rel="noreferrer noopener" target="_blank"><i className="fab fa-google-plus-square"></i></a>
+        <a href="https://www.instagram.com/jackyrusly/" rel="noreferrer noopener" target="_blank"><i className="fab fa-instagram"></i></a>
+        <a href="https://www.linkedin.com/in/jacky-rusly" rel="noreferrer noopener" target="_blank"><i className="fab fa-linkedin"></i></a>
 
     </div>
 );
